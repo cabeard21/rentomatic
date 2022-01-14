98af96982c726c60baba33c1f52546059635697d
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2022-01-14 20:34:50 +0000

    Chapter 6 finished.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from manage import read_json_configuration
+
 from application.app import create_app
 
 
@@ -8,3 +10,19 @@ def app():
     app = create_app("testing")
 
     return app
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--integration", action="store_true", help="run integration tests"
+    )
+
+
+def pytest_runtest_setup(item):
+    if "integration" in item.keywords and not item.config.getvalue("integration"):
+        pytest.skip("need --integration option to run")
+
+
+@pytest.fixture(scope="session")
+def app_configuration():
+    return read_json_configuration("testing")
